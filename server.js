ea2ac8e4d19c8f8a156f5ada797f1945b408ea7e
CedricA <cedric.j.aoun@gmail.com>
2026-03-10 11:49:39 +0100

    added file upload with formidable

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ require('dotenv').config();
 const port = process.env.PORT || 8000;
 const ejs = require('ejs');
 const path = require('path');
+const { formidable } = require('formidable');
+
+const cfg = {
+  dir: {
+    uploads: path.join(__dirname, 'uploads'),
+  },
+}
 
 app.set('view engine', 'ejs');
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/home', (req, res) => {
     res.render('home', { contactUrl: '/contact', aboutUrl: '/about' });
@@ -25,8 +33,30 @@ app.get('/form', (req, res) => {
   res.render('form', { title: "Form Test", data: req.query });
 });
 
-app.post('/form', (req, res) => {
-  res.render('form', { title: "Form Test", data: req.body });
+app.post('/form', (req, res, next) => {
+  const data = {};
+  const form = formidable({
+    uploadDir: cfg.dir.uploads,
+    keepExtensions: true,
+  });
+  form.parse(req, (err, fields, files) => {
+    data.name = fields?.name[0];
+    data.email = fields?.email[0];
+    if (err) {
+      next(err);
+      return;
+    }
+    if (files && files.image && files.image[0] && files.image[0].size > 0) {
+      const img = files.image[0];
+      console.dir(files, { depth: null, color: true });
+      data.filename = img.originalFilename;
+      data.filetype = img.mimetype;
+      data.filesize = Math.ceil(img.size / 1024) + 'KB';
+      data.uploadto = img.filepath;
+      data.imageurl = '/' + path.basename(img.filepath);
+    }
+    res.render('form', { title: 'Parse Form after image upload', data });
+  });
 });
 
 app.listen(port, () => {
